d9c93c00cafc87ce8397c556ac6eac185b7a186b
artemkuprianov <pokusaevaelina@gmail.com>
2026-06-09 15:06:51 +0600

    Зарегестрировал в админку

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import Status, Type, Issue
+
+
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Issue)
