804b72499b53e0f88056cbbe88e294f266d358c6
artemkuprianov <pokusaevaelina@gmail.com>
2026-06-09 15:04:19 +0600

    Created models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Status(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Issue(models.Model):
+    summary = models.CharField(
+        max_length=255,
+        verbose_name='Краткое описание'
+    )
+
+    description = models.TextField(
+        blank=True,
+        verbose_name='Полное описание'
+    )
+
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name='issues',
+        verbose_name='Статус'
+    )
+
+    type = models.ForeignKey(
+        Type,
+        on_delete=models.PROTECT,
+        related_name='issues',
+        verbose_name='Тип'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.summary
